ac3a22d075252ef01dbb251b381bc2cb77bb2ca5
Landon Trevisani <165218717+LandonTrev@users.noreply.github.com>
2024-04-17 19:59:45 -0400

    Update photo-organizer.py

--- photo-organizer.py
+++ photo-organizer.py
@@ -1,1 +1,7 @@
+import os  #Imports os module for file and directory operations
+from PIL import Image #Imports image module from the Pillow Library for image processing
+from PIL.Exiftags import TAGS #Imports TAGS from the Exiftags module in the Pillow Library for Exif data extraction
+import shutil # Imports shutil module for high-level file operations
+import time #Imports the time module. For measuring execution time of the program.
+
 
