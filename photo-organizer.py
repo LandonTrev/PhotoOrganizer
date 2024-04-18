a65e1ab2e3d85917064b84266a4bba0ab43b15ff
Landon Trevisani <165218717+LandonTrev@users.noreply.github.com>
2024-04-18 19:31:59 -0400

    Update photo-organizer.py
    Organized comments to show the flow of the program

--- photo-organizer.py
+++ photo-organizer.py
@@ -4,4 +4,28 @@ from PIL.Exiftags import TAGS #Imports TAGS from the Exiftags module in the Pill
 import shutil # Imports shutil module for high-level file operations
 import time #Imports the time module. For measuring execution time of the program.
 
+#Source folder 
+localPath = u' FOLDER PATH OF THE SOURCE FOLDER HERE'
+
+#Destination Folder 
+destinationPath = u'FOLDER PATH OF DESTINATION FOLDER HERE'
+
+# Mapping Exif tag names to their respective codes for retrieval
+
+# Global Variables to keep track of processing progress
+
+# Function to process to process a single photo
+def processphoto(photoPath):
+  global
+  try: 
+    with Image.open(photoPath) as im: 
+
+# Function to recursively process all files in a folder
+
+# Main function to initiate the processing
+
+
+      
+# Entry point of the script
+
 
